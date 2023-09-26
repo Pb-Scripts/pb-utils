b5736e279a7669418ffffddd0bca6bb80c21ed8e
Pedro Batista <pedro.batista.miguel9@gmail.com>
2023-09-26 14:37:31 +0100

    Update util_callback.lua

--- util_callback.lua
+++ util_callback.lua
@@ -1,3 +1,9 @@
 pb.callback.register('pb-utils:removeItem', function(source, item, count, inv, metadata, slot)
     return exports.ox_inventory:RemoveItem(inv or source, item, count, metadata, slot)
 end)
+
+pb.callback.register('pb-utils:removeBankMoney', function(source, money)
+    local qb = exports['qb-core']:GetCoreObject()
+    local Player = qb.Functions.GetPlayer(source)
+    return Player.Functions.RemoveMoney("bank", money)
+end)
