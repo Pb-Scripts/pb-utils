5a07c4ca523552371b419c39e16810d29f033e2e
Pedro Batista <pedro.batista.miguel9@gmail.com>
2023-09-27 09:31:18 +0100

    fix play anim

--- modules/playAnim/client.lua
+++ modules/playAnim/client.lua
@@ -2,7 +2,7 @@ function pb.playAnim(dict, anim, flag, time)
     local ped = PlayerPedId()
     while (not HasAnimDictLoaded(dict)) do RequestAnimDict(dict) Wait(0) end
     TaskPlayAnim(ped, dict, anim, 8.0, -8, -1, flag, 0, 0, 0, 0)
-    if time then Wait(time) ClearPedTasksImmediately(ped) end
+    if time then Wait(time) StopAnimTask(ped, dict, anim, 500) end
     return
 end
 
