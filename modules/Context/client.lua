077d9efbc09a57e3c23b5ef5afe6dc017416f317
Pedro Batista <pedro.batista.miguel9@gmail.com>
2023-09-27 11:41:57 +0100

    add menus

---
+++ modules/Context/client.lua
@@ -0,0 +1,11 @@
+pb.Context = {
+    registerContext = function(options)
+        lib.registerContext(options)
+    end,
+
+    showContext = function(id)
+        lib.showContext(id)
+    end,
+}
+
+return pb.Context
---
+++ modules/Radial/client.lua
@@ -0,0 +1,12 @@
+pb.Radial = {
+    
+    addRadialItem = function(options)
+        lib.addRadialItem(options)
+    end,
+
+    removeRadialItem = function(id)
+        lib.removeRadialItem(id)
+    end,
+}
+
+return pb.Radial
